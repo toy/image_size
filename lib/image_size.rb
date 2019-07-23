90f913ca87bfe8c37c30b210c1c89ba50a47b232
Ivan Kuchin <tadump+git@gmail.com>
2019-07-23 19:22:16 +0200

    rubocop: stop 0.73 complaining about then in multiline when

--- lib/image_size.rb
+++ lib/image_size.rb
@@ -84,28 +84,27 @@ class ImageSize
 private
 
   SVG_R = /<svg\b([^>]*)>/.freeze
+  XML_R = /<\?xml|<!--/.freeze
   def detect_format(ir)
     head = ir[0, 1024]
     case
-    when head[0, 6] =~ /GIF8[79]a/          then :gif
-    when head[0, 8] == "\211PNG\r\n\032\n"  then detect_png_type(ir)
-    when head[0, 8] == "\212MNG\r\n\032\n"  then :mng
-    when head[0, 2] == "\377\330"           then :jpeg
-    when head[0, 2] == 'BM'                 then :bmp
-    when head[0, 2] =~ /P[1-7]/             then :ppm
-    when head =~ /\#define\s+\S+\s+\d+/     then :xbm
-    when head[0, 4] == "II*\000"            then :tiff
-    when head[0, 4] == "MM\000*"            then :tiff
-    when head =~ %r{/\* XPM \*/}            then :xpm
-    when head[0, 4] == '8BPS'               then :psd
-    when head[0, 3] =~ /[FC]WS/             then :swf
-    when head[SVG_R] ||
-      head =~ /<\?xml|<!--/ && ir[0, 4096][SVG_R]
-                                            then :svg
-    when head[0, 2] =~ /\n[\000-\005]/      then :pcx
-    when head[0, 12] =~ /RIFF(?m:....)WEBP/ then :webp
-    when head[0, 4] == "\000\000\001\000"   then :ico
-    when head[0, 4] == "\000\000\002\000"   then :cur
+    when head[0, 6] =~ /GIF8[79]a/                                then :gif
+    when head[0, 8] == "\211PNG\r\n\032\n"                        then detect_png_type(ir)
+    when head[0, 8] == "\212MNG\r\n\032\n"                        then :mng
+    when head[0, 2] == "\377\330"                                 then :jpeg
+    when head[0, 2] == 'BM'                                       then :bmp
+    when head[0, 2] =~ /P[1-7]/                                   then :ppm
+    when head =~ /\#define\s+\S+\s+\d+/                           then :xbm
+    when head[0, 4] == "II*\000"                                  then :tiff
+    when head[0, 4] == "MM\000*"                                  then :tiff
+    when head =~ %r{/\* XPM \*/}                                  then :xpm
+    when head[0, 4] == '8BPS'                                     then :psd
+    when head[0, 3] =~ /[FC]WS/                                   then :swf
+    when head =~ SVG_R || (head =~ XML_R && ir[0, 4096][SVG_R])   then :svg
+    when head[0, 2] =~ /\n[\000-\005]/                            then :pcx
+    when head[0, 12] =~ /RIFF(?m:....)WEBP/                       then :webp
+    when head[0, 4] == "\000\000\001\000"                         then :ico
+    when head[0, 4] == "\000\000\002\000"                         then :cur
     end
   end
 
